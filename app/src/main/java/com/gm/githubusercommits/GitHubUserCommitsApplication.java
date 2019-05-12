54bd8f1cf0f78411c6d058041b81610c851060e3
madhur <madhusudhana.ravulapalli@kpit.com>
2019-05-12 03:57:09 -0400

    Add Application class

---
+++ app/src/main/java/com/gm/githubusercommits/GitHubUserCommitsApplication.java
@@ -0,0 +1,27 @@
+package com.gm.githubusercommits;
+
+import android.app.Application;
+
+import com.gm.githubusercommits.dagger.component.GitHubUserCommitsComponent;
+import com.gm.githubusercommits.dagger.modules.GitHubUserCommitsModule;
+
+/**
+ * Created by madhu on 5/12/19.
+ */
+
+public class GitHubUserCommitsApplication extends Application {
+    private GitHubUserCommitsComponent component;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        component = DaggerGitHubUserCommitsComponent.
+                builder().
+                gitHubUserCommitsModule(new GitHubUserCommitsModule(getApplicationContext()))
+                .build();
+    }
+
+    public GitHubUserCommitsComponent getComponent() {
+        return component;
+    }
+}
