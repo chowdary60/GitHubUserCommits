e712a79c45fc4d16f4f35db08eea2569698fd2a7
madhur <madhusudhana.ravulapalli@kpit.com>
2019-05-12 03:10:16 -0400

    add github user contract

---
+++ app/src/main/java/com/gm/githubusercommits/contracts/GitHubUserCommitsContract.java
@@ -0,0 +1,27 @@
+package com.gm.githubusercommits.contracts;
+
+import com.gm.githubusercommits.models.GitHubCommit;
+
+import java.util.List;
+
+/**
+ * Created by madhu on 5/12/19.
+ */
+
+public interface GitHubUserCommitsContract {
+    public interface View {
+        void showProgressDialog();
+
+        void hideProgressDialog();
+
+        void processResponse(List<GitHubCommit> response);
+
+        void processError(Throwable exception);
+
+        void connectionNotAvailable();
+    }
+
+    public interface Presenter {
+        void onStart();
+    }
+}
