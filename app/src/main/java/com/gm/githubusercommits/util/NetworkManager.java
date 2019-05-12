1fc018168b6dbbe41fdd75c640e585226173df69
madhur <madhusudhana.ravulapalli@kpit.com>
2019-05-12 02:53:14 -0400

    check internet connect for the application

--- app/src/main/java/com/gm/githubusercommits/util/NetworkManager.java
+++ app/src/main/java/com/gm/githubusercommits/util/NetworkManager.java
@@ -1,6 +1,7 @@
 package com.gm.githubusercommits.util;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
 
 /**
  * Created by madhu on 5/12/19.
@@ -10,5 +11,9 @@ public class NetworkManager {
     private Context context;
     public NetworkManager(Context context) {
         this.context = context;
+    }
+    public boolean isInternetConnectionAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connectivityManager.getActiveNetworkInfo() != null;
     }
 }
